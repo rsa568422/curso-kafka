96e099d5c8503138b3b5b06136a1c6ad584f1ee0
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2022-09-04 21:23:27 +0200

    23. Producer callbacks utilizando clases anónimas
    Se implementa un productor con método callback empleando para ello una clase anónima.

---
+++ src/main/java/com/devs4j/kafka/callbacks/Devs4jCallbackProducer.java
@@ -0,0 +1,46 @@
+package com.devs4j.kafka.callbacks;
+
+import org.apache.kafka.clients.producer.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class Devs4jCallbackProducer {
+
+    public static final Logger log = LoggerFactory.getLogger(Devs4jCallbackProducer.class);
+
+    public static void main(String[] args) {
+
+        long startTime = System.currentTimeMillis();
+
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092"); // broker de kafka al que vamos a conectar
+        properties.put("acks", "all"); // 1 para asegurar que un nodo recibe el mensaje, all para que todos lo reciban
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer"); // serialización
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer"); // serialización
+        properties.put("linger.ms", "10"); // serialización
+
+        try (Producer<String, String> producer = new KafkaProducer<>(properties)) {
+            for (int i = 0; i < 10000; i++) {
+                producer.send(new ProducerRecord<>("devs4j-topic", String.valueOf(i), "devs4j-value"), new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata recordMetadata, Exception exception) {
+                        if (exception != null) {
+                            log.error("There was an error {}", exception.getMessage());
+                        }
+                        log.info("Offset = {}, Partition = {}, Topic = {}",
+                                recordMetadata.offset(),
+                                recordMetadata.partition(),
+                                recordMetadata.topic());
+                    }
+                });
+            }
+            producer.flush();
+        }
+
+        log.info("Processing time = {} ms", System.currentTimeMillis() - startTime);
+
+    }
+
+}
