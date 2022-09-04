e90f132e36f57c8f410b27be998e77870b1a948a
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2022-09-04 15:32:34 +0200

    15. Mejorando performance de los producers
    Se agrega la clave linger.ms a las propiedades del producer para comparar rendimiento con distintos valores, llegando a la conclusión de que en nuestro ejemplo el valor óptimo es 10.

--- src/main/java/com/devs4j/kafka/producers/Devs4jProducer.java
+++ src/main/java/com/devs4j/kafka/producers/Devs4jProducer.java
@@ -15,20 +15,25 @@ public class Devs4jProducer {
 
     public static void main(String[] args) {
 
+        long startTime = System.currentTimeMillis();
+
         Properties properties = new Properties();
         properties.put("bootstrap.servers", "localhost:9092"); // broker de kafka al que vamos a conectar
         properties.put("acks", "1"); // 1 para asegurar que un nodo recibe el mensaje, all para que todos lo reciban
         properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer"); // serialización
         properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer"); // serialización
+        properties.put("linger.ms", "10"); // serialización
 
         try(Producer<String, String> producer = new KafkaProducer<>(properties)) {
-            for (int i = 0; i < 10000; i++) {
+            for (int i = 0; i < 1000000; i++) {
                 producer.send(new ProducerRecord<>("devs4j-topic", String.valueOf(i), "devs4j-value")).get();
             }
             producer.flush();
         } catch (InterruptedException | ExecutionException e) {
             log.error("Message producer interrupted ", e);
         }
+
+        log.info("Processing time = {} ms", System.currentTimeMillis() - startTime);
 
     }
 
