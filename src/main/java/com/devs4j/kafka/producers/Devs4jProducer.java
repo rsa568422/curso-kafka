afaf0e8ed62baa45d0a241e00507d161abd002d6
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2022-09-04 14:18:15 +0200

    14. Producer síncrono vs asíncrono
    Se modifica el producer de la clase anterior para hacer que el envío sea síncrono.
    
    Esto hace más lento el envío, pero los mensajes llegan ordenados.

--- src/main/java/com/devs4j/kafka/producers/Devs4jProducer.java
+++ src/main/java/com/devs4j/kafka/producers/Devs4jProducer.java
@@ -3,10 +3,15 @@ package com.devs4j.kafka.producers;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class Devs4jProducer {
+
+    public static final Logger log = LoggerFactory.getLogger(Devs4jProducer.class);
 
     public static void main(String[] args) {
 
@@ -17,7 +22,12 @@ public class Devs4jProducer {
         properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer"); // serialización
 
         try(Producer<String, String> producer = new KafkaProducer<>(properties)) {
-            producer.send(new ProducerRecord<>("devs4j-topic", "devs4j-key", "devs4j-value"));
+            for (int i = 0; i < 10000; i++) {
+                producer.send(new ProducerRecord<>("devs4j-topic", String.valueOf(i), "devs4j-value")).get();
+            }
+            producer.flush();
+        } catch (InterruptedException | ExecutionException e) {
+            log.error("Message producer interrupted ", e);
         }
 
     }
