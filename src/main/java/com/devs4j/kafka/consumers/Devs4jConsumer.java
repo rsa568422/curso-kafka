b432abd6bd2c7976956aa36656dd65e0e75a6b9f
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2022-09-04 23:13:52 +0200

    31. Commit de los mensajes de forma manual
    Se establece al autocommit de las propiedades del consumidor a false, realizando el commit manualmente tras procesar el mensaje.

--- src/main/java/com/devs4j/kafka/consumers/Devs4jConsumer.java
+++ src/main/java/com/devs4j/kafka/consumers/Devs4jConsumer.java
@@ -1,15 +1,14 @@
 package com.devs4j.kafka.consumers;
 
-import com.devs4j.kafka.producers.Devs4jProducer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
-import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 public class Devs4jConsumer {
@@ -25,11 +24,10 @@ public class Devs4jConsumer {
         properties.setProperty("auto.commit.interval.ms","1000");
         properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 
         try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties)) {
-            TopicPartition topicPartition = new TopicPartition("devs4j-topic", 0);
-            consumer.assign(Arrays.asList(topicPartition));
-            consumer.seek(topicPartition, 50);
+            consumer.subscribe(List.of("devs4j-topic"));
             while (true) {
                 ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(100));
                 for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
@@ -38,6 +36,7 @@ public class Devs4jConsumer {
                             consumerRecord.partition(),
                             consumerRecord.key(),
                             consumerRecord.value());
+                    consumer.commitSync();
                 }
             }
         }
